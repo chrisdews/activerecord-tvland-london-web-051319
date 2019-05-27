b2deca2d28a39634dfbd32a9de534447cbaa6423
chrisdews <cldews1@gmail.com>
2019-05-27 11:05:05 +0100

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+ #  	def build_network(hash)
+	# 	# self.network_id = network.id
+	# end
 end
+
